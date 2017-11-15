67f18ddbf06919ec9c9dc97a030cece00b17357f
Araba Taylor <araba.taylor20@gmail.com>
2017-11-15 02:38:48 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,6 +66,6 @@ attr_reader :species
 
   def list_pets
     binding.pry
-      "I have 2 fish, 3 dog(s), and 1 cat(s)."
+      "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length}dog(s), and 1 cat(s)."
   end
 end
